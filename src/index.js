3669042cf3321e3eb85ccb760d73e5673fcfe954
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-07-25 14:58:45 +0100

    added all cities function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-//It is Saturday, October 8, 2023 2:51 PM in Europe/Paris
-
-/*function showTime(event) {
-  if (event.target.value === "paris") {
-    let parisTime = moment()
-      .tz("Europe/Paris")
-      .format("dddd, MMMM D, YYYY h:m A");
-    alert(`It is ${parisTime} in Europe/Paris`);
-  }
-  if (event.target.value === "tokyo") {
-    let tokyoTime = moment()
-      .tz("Asia/Tokyo")
-      .format("dddd, MMMM D, YYYY h:m A");
-    alert(`It is ${tokyoTime} in Asia/Tokyo`);
-  }
-  if (event.target.value === "sydney") {
-    let sydneyTime = moment()
-      .tz("Australia/Sydney")
-      .format("dddd, MMMM D, YYYY h:m A");
-    alert(`It is ${sydneyTime} in Australia/Sydney`);
-  }
-}
-let countriesSelect = document.querySelector("#countries");
-countriesSelect.addEventListener("change", showTime);*/
-
 function updateTime() {
   let londonDate = document.querySelector("#london .date");
   let londonTime = document.querySelector("#londonTime");
@@ -76,7 +51,9 @@ function changeCity(event) {
           <div class="time" >
             ${cityTime.format("h:mm:ss [<small>]A[</small>]")}
           </div>
-        </div>`;
+          
+        </div>
+        <a href="index.html" class="allCities">All cities</a>`;
 }
 //setInterval(changeCity, 1000);
 updateTime();
